5a1d59e7a946a22dc5f789a18c23de8f46ed7c14
Otto William Peyer IV <opeyer@gmail.com>
2021-06-12 05:30:54 -0500

    Began work on MITx 6.00.1 and PY4E.

---
+++ MITx/helloworld.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 05:40:00 2021
+
+@author: opeyer
+"""
+
+print('hello world')
+print('Gee wilikers, I sure do like 6.00.1x!')
---
+++ MITx/untitled0.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 05:40:00 2021
+
+@author: opeyer
+"""
+
+print('hello world')
