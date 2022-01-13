51688d4da47a100e24efbcd69d873177d35ff37f
Otto William Peyer IV <opeyer@gmail.com>
2022-01-13 03:24:42 -0600

    First exercise in the first MSDotNet Learn C# tutorial.

---
+++ MS-Dotnet-Tutorial/Learn-C#/01_take-your-first-steps-with-C#/01_hello-world.cs
@@ -0,0 +1,8 @@
+// Console.WriteLine("Hello World!");
+
+Console.Write("Congratulations!");
+Console.Write("  ");
+Console.Write("You wrote your first lines of code!");
+
+// The first technique seen here used Console.WriteLine(), which added a line feed at the end of the line.
+// The second technique used Console.Write(), which functions similarly but does *not* end with a line feed.
