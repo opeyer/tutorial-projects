50c626aa7bd5632efb8f2af369fa7df3378e5269
Otto William Peyer IV <opeyer@gmail.com>
2022-01-21 03:55:26 -0600

    Preliminary work on the Rust "guessing game".

---
+++ code/Rust/3_guessing-game/guessing_game/target/debug/deps/guessing_game-944b310f5f7472e0.d
@@ -0,0 +1,5 @@
+/mnt/e/_doc/github/tutorial-projects/code/Rust/3_guessing-game/guessing_game/target/debug/deps/guessing_game-944b310f5f7472e0: src/main.rs
+
+/mnt/e/_doc/github/tutorial-projects/code/Rust/3_guessing-game/guessing_game/target/debug/deps/guessing_game-944b310f5f7472e0.d: src/main.rs
+
+src/main.rs:
---
+++ code/Rust/3_guessing-game/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/mnt/e/_doc/github/tutorial-projects/code/Rust/3_guessing-game/guessing_game/target/debug/guessing_game: /mnt/e/_doc/github/tutorial-projects/code/Rust/3_guessing-game/guessing_game/src/main.rs
