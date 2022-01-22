82e2cee08bdf496dec0c6dacfc25758ab7807c1c
Otto William Peyer IV <opeyer@gmail.com>
2022-01-21 20:56:22 -0600

    Axed comments in Rust: Guessing Game. Initial work on Rust: Variables.

---
+++ code/Rust/4_variables/variables/target/debug/deps/variables-6d9a1caa7d983c1b.d
@@ -0,0 +1,5 @@
+/mnt/e/_doc/github/tutorial-projects/code/Rust/4_variables/variables/target/debug/deps/variables-6d9a1caa7d983c1b: src/main.rs
+
+/mnt/e/_doc/github/tutorial-projects/code/Rust/4_variables/variables/target/debug/deps/variables-6d9a1caa7d983c1b.d: src/main.rs
+
+src/main.rs:
