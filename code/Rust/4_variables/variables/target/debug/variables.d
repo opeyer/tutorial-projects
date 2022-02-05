b14e9980013d98d86b337f6ffc2c880b4fbc9767
Otto William Peyer IV <opeyer@gmail.com>
2022-02-05 05:50:36 -0600

    Renaming scheme for Rust projects.

--- code/Rust/4_variables/variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/mnt/e/_doc/github/tutorial-projects/code/Rust/4_variables/variables/target/debug/variables: /mnt/e/_doc/github/tutorial-projects/code/Rust/4_variables/variables/src/main.rs
---
+++ code/Rust/variables/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/mnt/e/_doc/github/tutorial-projects/code/rust/4_variables/variables/target/debug/variables: /mnt/e/_doc/github/tutorial-projects/code/rust/4_variables/variables/src/main.rs
