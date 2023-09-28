5453955b0db840a282e60df3e674a3f221a62e90
Otto William Peyer IV <opeyer@gmail.com>
2023-09-28 00:28:04 -0500

    Added header to Greeting Card

--- python/greeting-card/greeting-card.py
+++ python/greeting-card/greeting-card.py
@@ -1,2 +1,13 @@
-name = input("Enter your name")
-print("Hello", name)
+# Elevator Conversion Tool
+# Pre-alpha version
+# by Otto William Peyer IV
+
+# Header
+print('========================')
+print('     GREETING CARD      ')
+print('by   opeyer @ opeyer.com')
+print('========================')
+
+# Function
+name = input("Enter your name: ")
+print("Hello,", name, "!")
