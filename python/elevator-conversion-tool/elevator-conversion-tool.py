73f811d1eb0981423416e470e4019ce9d0d445fa
Otto William Peyer IV <opeyer@gmail.com>
2023-09-28 00:23:36 -0500

    Added header

---
+++ python/elevator-conversion-tool/elevator-conversion-tool.py
@@ -0,0 +1,14 @@
+# Elevator Conversion Tool
+# Pre-alpha version
+# by Otto William Peyer IV
+
+# Header
+print('========================')
+print('ELEVATOR CONVERSION TOOL')
+print('by   opeyer @ opeyer.com')
+print('========================')
+
+# Function (Europe to US)
+inp = input('Europe floor? ')
+usf = int(inp) + 1
+print('If you were in the US, it would be floor no.', usf, '!')
--- python/elevator-converter/elevator-converter.py
+++
@@ -1,3 +0,0 @@
-inp = input('Europe floor? ')
-usf = int(inp) + 1
-print('In the US, it would be floor no.', usf, '.')
