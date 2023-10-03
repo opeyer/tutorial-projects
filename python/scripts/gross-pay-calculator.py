813b4e0cd5c880181c2185c9f86e6d0714cd17c3
Otto William Peyer IV <opeyer@gmail.com>
2023-10-03 09:51:54 -0500

    try/except support for Gross Pay Calculator

--- python/scripts/gross-pay-calculator.py
+++ python/scripts/gross-pay-calculator.py
@@ -12,14 +12,16 @@ print('========================')
 hours = input("Enter hours: ")
 rate = input("Enter rate (in $USD per hour): ")
 
-hoursInt = int(hours)
 
-if hoursInt <= 40:
-    gross = float(hours) * float(rate)
-elif hoursInt > 40:
-    grossPreOvertime = 40 * float(rate)
-    overtime = (float(hours) - 40)
-    overtimePay = (float(overtime) * (float(rate) * 1.5))    
-    gross = grossPreOvertime + overtimePay
-
-print("Your pay is $", gross, "!")
+try:
+    hoursInt = int(hours)
+    if hoursInt <= 40:
+        gross = float(hours) * float(rate)
+    elif hoursInt > 40:
+        grossPreOvertime = 40 * float(rate)
+        overtime = (float(hours) - 40)
+        overtimePay = (float(overtime) * (float(rate) * 1.5))    
+        gross = grossPreOvertime + overtimePay
+    print("Your pay is $", gross, "!")
+except:
+    print("Error! Please enter numeric input.")
