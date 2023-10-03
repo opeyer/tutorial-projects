361920889902024ba2bbdababacdb01ac1476d77
Otto William Peyer IV <opeyer@gmail.com>
2023-10-03 09:45:36 -0500

    Added overtime support to Gross Pay Calculator

--- python/scripts/gross-pay-calculator.py
+++ python/scripts/gross-pay-calculator.py
@@ -12,6 +12,14 @@ print('========================')
 hours = input("Enter hours: ")
 rate = input("Enter rate (in $USD per hour): ")
 
-gross = float(hours) * float(rate)
+hoursInt = int(hours)
+
+if hoursInt <= 40:
+    gross = float(hours) * float(rate)
+elif hoursInt > 40:
+    grossPreOvertime = 40 * float(rate)
+    overtime = (float(hours) - 40)
+    overtimePay = (float(overtime) * (float(rate) * 1.5))    
+    gross = grossPreOvertime + overtimePay
 
 print("Your pay is $", gross, "!")
