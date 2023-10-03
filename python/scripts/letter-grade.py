a7bc909d8119f7199a514d8a076ba858424024e6
Otto William Peyer IV <opeyer@gmail.com>
2023-10-03 11:17:25 -0500

    Created Letter Grade script

---
+++ python/scripts/letter-grade.py
@@ -0,0 +1,31 @@
+# Letter Grade
+# Pre-alpha version
+# by Otto William Peyer IV
+
+# Header
+print('========================')
+print('    LETTER     GRADE    ')
+print('by   opeyer @ opeyer.com')
+print('========================')
+
+# Function
+gradeScore = input("Enter your score (accepted range: 0.0-1.0): ")
+gradeScoreFloat = float(gradeScore)
+    
+try:
+    if gradeScoreFloat < 0.0:
+        print("Your value is under 0.0! Please enter a numeric value from 0.0 to 1.0.")
+    elif gradeScoreFloat > 1.0:
+        print("Your value is over 1.0! Please enter a numeric value from 0.0 to 1.0.")
+    elif gradeScoreFloat >= 0.9:
+        print("A")
+    elif gradeScoreFloat >= 0.8:
+        print("B")
+    elif gradeScoreFloat >= 0.7:
+        print("C")
+    elif gradeScoreFloat >= 0.6:
+        print("D")
+    elif gradeScoreFloat < 0.6:
+        print("F")
+except:
+    print("Your value is not a numeric value! Please enter a numeric value from 0.0 to 1.0.")
